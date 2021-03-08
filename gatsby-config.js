8407cf758080b9d6a6b01096467ea575498d5bec
MuhammdSheraz <mshazy999@gmail.com>
2021-03-08 18:02:28 +0500

    fix deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,15 @@
-  
-// var baseUrl =
-// process.env.NODE_ENV === "development"
-//   ? "http://localhost:8888"
-//   : "https://adareyou654321.netlify.app";
 
-// module.exports = {
-//   /* Your site config here */
-//   plugins: [
-//     "gatsby-plugin-postcss",
-//     {
-//       resolve: "gatsby-source-graphql",
-//       options: {
-//         typeName: "Lollies",
-//         fieldName: "Lollies",
-//         url: `https://shazy-virtual-lolly.netlify.app/.netlify/functions/lollyCreate`,
-//       },
-//     },
-//   ],
-// }
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "Lollies",
+        fieldName: "Lollies",
+        url: `https://shazy-virtual-lolly.netlify.app/.netlify/functions/lollyCreate`,
+      },
+    },
+  ],
+}
