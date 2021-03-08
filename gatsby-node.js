1f11609bb69eca44dfe4e765dda3c755fe6dc31a
MuhammdSheraz <mshazy999@gmail.com>
2021-03-08 17:58:38 +0500

    fix deployment

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,42 @@
-const path = require(`path`)
+// const path = require(`path`)
 
-exports.createPages = async ({ page, actions, graphql }) => {
-  try {
-    const { data } = await graphql(`
-      query MyQuery {
-        Lollies {
-          getLollyCards {
-            Id
-            color1
-            color2
-            color3
-            messageBody
-            to
-            from
-          }
-        }
-      }
-    `)
-    console.log("data===>", data.Lollies.getLollyCards)
-    data.Lollies.getLollyCards.map(
-      ({ Id, color1, color2, color3, messageBody, to, from }) => {
-        console.log("Id ===>", Id)
-        actions.createPage({
-          path: `lolly/${Id}`,
-          component: require.resolve(`./src/Components/LollyTemplate`),
-          context: {
-            Id: Id,
-            color1: color1,
-            color2: color2,
-            color3: color3,
-            messageBody: messageBody,
-            to: to,
-            from: from,
-          },
-        })
-      }
-    )
-  } catch (err) {
-    console.log(err.message)
-  }
-}
+// exports.createPages = async ({ page, actions, graphql }) => {
+//   try {
+//     const { data } = await graphql(`
+//       query MyQuery {
+//         Lollies {
+//           getLollyCards {
+//             Id
+//             color1
+//             color2
+//             color3
+//             messageBody
+//             to
+//             from
+//           }
+//         }
+//       }
+//     `)
+//     console.log("data===>", data.Lollies.getLollyCards)
+//     data.Lollies.getLollyCards.map(
+//       ({ Id, color1, color2, color3, messageBody, to, from }) => {
+//         console.log("Id ===>", Id)
+//         actions.createPage({
+//           path: `lolly/${Id}`,
+//           component: require.resolve(`./src/Components/LollyTemplate`),
+//           context: {
+//             Id: Id,
+//             color1: color1,
+//             color2: color2,
+//             color3: color3,
+//             messageBody: messageBody,
+//             to: to,
+//             from: from,
+//           },
+//         })
+//       }
+//     )
+//   } catch (err) {
+//     console.log(err.message)
+//   }
+// }
